9c090895145ee43ec3124ab932870a5d44b1c6fa
JR <josdonare@alu.edu.gva.es>
2025-10-24 10:29:36 +0200

    he cambiado a francisco por clases de git

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Woooorld!");
-        System.out.println("Hello, Francisco");
+        System.out.println("CLASES DE GIT");
     }
 }
